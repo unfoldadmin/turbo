889915c815a4c4c0db0706e2b857dc55291d12ba
lukas <lukasvinclav@gmail.com>
2024-12-29 10:58:21 +0100

    feat: removed src/ folder in web

--- frontend/apps/web/tailwind.config.js
+++ frontend/apps/web/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '../../packages/ui/forms/**/*.{js,ts,jsx,tsx,mdx}',
-    '../../packages/ui/messages/**/*.{js,ts,jsx,tsx,mdx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/forms/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/messages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
