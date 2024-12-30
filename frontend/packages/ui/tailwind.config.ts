a112bff0ff5bb0da989029dd483ed8642c91ed32
lukas <lukasvinclav@gmail.com>
2024-12-30 11:12:55 +0100

    feat: pre-commit biome support

--- frontend/packages/ui/tailwind.config.ts
+++ frontend/packages/ui/tailwind.config.ts
@@ -47,8 +47,8 @@ const config = {
         }
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
       }
     }
