f850f7a2baa078e429667ee1ab8b651d40f7cd23
root <joan@ruichi-sz.com>
2025-08-11 16:46:43 +0300

    core add

--- apps/backend/api/urls.py
+++ apps/backend/api/urls.py
@@ -4,11 +4,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .api import UserViewSet, RFQViewSet
+from .api import UserViewSet
 
 router = routers.DefaultRouter()
 router.register("users", UserViewSet, basename="api-users")
-router.register("rfqs", RFQViewSet, basename="api-rfqs")
 
 urlpatterns = [
     path(
